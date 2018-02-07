e0f5bc5a344d6f0b751089990bb5c130ea8cfc98
Kelvin Li <jpkelvinli@gmail.com>
2018-02-07 01:23:39 -0800

    Change webpack build to include minified version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './src/bundle.js',
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'flexgl.js'
-  }
+    entry: {
+        "flexgl": "./src/bundle.js",
+        "flexgl.min": "./src/bundle.js"
+    },
+    devtool: "inline-source-map",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            include: /\.min\.js$/,
+            sourceMap: true
+        })
+    ]
 };
